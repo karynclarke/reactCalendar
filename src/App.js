a5746c0a995e063b41f2ed97653fd639d3eb0386
karynclarke <karynclarke@comcast.net>
2020-10-04 11:58:17 -0700

    31 days

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, {Component} from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import Calendar from "./Components/Calendar";
+import Calendar from './Components/Calendar';
 
 const style = {
-  position: "relative",
-  margin: "50px auto"
+	position: 'relative',
+	margin: '50px auto'
 }
 
-
-function App() {
-  return (
-    <div className="App">
-      <Calendar style = {style} width = "302px"/>
-    </div>
-  );
+class App extends Component {
+  onDayClick = (e, day) => {
+    alert (day);
+  }
+  
+	render() {
+		return (
+			<div className="App">
+        <Calendar style={style} width="302px" 
+        onDayClick= {(e, day) => this.onDayClick(e,day)} />
+			</div>
+		);
+	}
 }
 
 export default App;
